daaca9d5aad6955f0608507ed9b9e04d859f069b
InaliQ <hernandez.medi.eduardo19@gmail.com>
2024-10-24 17:33:21 -0600

    Controller añadidos

--- Back_health/Models/HealtPlusContext.cs
+++ Back_health/Models/HealtPlusContext.cs
@@ -41,6 +41,7 @@ public partial class HealtPlusContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=ADAGAKIAKILAP; Initial Catalog=healt_plus; user id=Inali; password=InaliQ01;TrustServerCertificate=true");
@@ -61,7 +62,7 @@ public partial class HealtPlusContext : DbContext
             entity.Property(e => e.FechaHora)
                 .HasColumnType("datetime")
                 .HasColumnName("fecha_hora");
-        });
+        }); 
 
         modelBuilder.Entity<Doctor>(entity =>
         {
